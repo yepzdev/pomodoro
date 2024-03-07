e026e1ac4a04c2cfa02149e7c1d2b8fcf9ec44b5
alfredo <alfaradayepez@gmail.com>
2024-03-07 12:56:58 -0500

    separating buttons to a separate file

---
+++ js/buttons.js
@@ -0,0 +1,45 @@
+export let button = $("<button>");
+
+// start
+$(button)
+.clone()
+.attr("id", "start")
+.addClass("start-btn")
+.text("Start")
+.appendTo(".pomodoro-button-wrapper.footer");
+
+// reset 
+$(button)
+.clone()
+.attr("id", "reset")
+.addClass("reset-btn")
+.text("Reset")
+.appendTo(".pomodoro-button-wrapper.footer");
+
+// pomodoro
+$(button)
+.clone()
+.attr("id", "pomodoro-btn")
+.text("Pomodoro")
+.appendTo(".pomodoro-button-wrapper.header");
+
+// long break
+$(button)
+.clone()
+.attr("id", "long-break-btn")
+.text("Long Break")
+.appendTo(".pomodoro-button-wrapper.header");
+
+// short break
+$(button)
+.clone()
+.attr("id", "short-break-btn")
+.text("Short Break")
+.appendTo(".pomodoro-button-wrapper.header");
+
+// add task
+$(button)
+.clone()
+.attr("id", "add-task")
+.text("Add task")
+.appendTo(".task-container");
--- js/index.js
+++ js/index.js
@@ -1,118 +1,71 @@
-(() => {
-  "use strict";
+"use strict";
 
-  let button = $("<button>"),
-    task_field = $("<input>");
+import * as buttons from "./buttons.js";
 
-  task_field.clone()
-    .attr("id", "task-field")
-    .appendTo(".task-container");
+// task list field
+let task_field = $("<input>");
+task_field.clone().attr("id", "task-field").appendTo(".task-container");
 
-  $(button)
-    .clone()
-    .attr("id", "start")
-    .addClass("start-btn")
-    .text("Start")
-    .appendTo(".pomodoro-button-wrapper.footer");
+$(document).ready(() => {
+  let timer;
+  let timeLeft = 25 * 60; // 25 minutes in seconds
+  let isPaused = true;
 
-  // reset button
-  $(button)
-    .clone()
-    .attr("id", "reset")
-    .addClass("reset-btn")
-    .text("Reset")
-    .appendTo(".pomodoro-button-wrapper.footer");
+  function formatTime(seconds) {
+    let minutes = Math.floor(seconds / 60);
+    let remainingSeconds = seconds % 60;
+    return `${minutes < 10 ? "0" : ""}${minutes}:${
+      remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  }
 
-  // pomodoro button
-  $(button)
-    .clone()
-    .attr("id", "pomodoro-btn")
-    .text("Pomodoro")
-    .appendTo(".pomodoro-button-wrapper.header");
+  const updateTimer = () => {
+    $("#timer").text(formatTime(timeLeft));
+  }
 
-  // long break button
-  $(button)
-    .clone()
-    .attr("id", "long-break-btn")
-    .text("Long Break")
-    .appendTo(".pomodoro-button-wrapper.header");
-
-  // short break button
-  $(button)
-    .clone()
-    .attr("id", "short-break-btn")
-    .text("Short Break")
-    .appendTo(".pomodoro-button-wrapper.header");
-
-  // add task button
-  $(button)
-    .clone()
-    .attr("id", "add-task")
-    .text("Add task")
-    .appendTo(".task-container");
-
-  $(document).ready(() => {
-    let timer;
-    let timeLeft = 25 * 60; // 25 minutes in seconds
-    let isPaused = true;
-
-    function formatTime(seconds) {
-      let minutes = Math.floor(seconds / 60);
-      let remainingSeconds = seconds % 60;
-      return `${minutes < 10 ? "0" : ""}${minutes}:${
-        remainingSeconds < 10 ? "0" : ""
-      }${remainingSeconds}`;
-    }
-
-    function updateTimer() {
-      $("#timer").text(formatTime(timeLeft));
-    }
-
-    function toggleTimer() {
-      if (isPaused) {
-        timer = setInterval(() => {
-          timeLeft--;
-          updateTimer();
-          if (timeLeft === 0) {
-            clearInterval(timer);
-            alert("¡Tiempo terminado!");
-          }
-        }, 1000);
-        isPaused = false;
-        $("#start").text("Pause");
-      } else {
-        clearInterval(timer);
-        isPaused = true;
-        $("#start").text("Start");
-      }
-    }
-
-    $("#short-break-btn").click(() => {
-      resetInterval(5);
-    });
-
-    $("#long-break-btn").click(() => {
-      resetInterval(15);
-    });
-
-    $("#pomodoro-btn").click(() => {
-      resetInterval(25);
-    });
-
-    $("#start").click(() => toggleTimer());
-
-    $("#reset").click(() => {
-      resetInterval(25);
-    });
-
-    updateTimer();
-
-    const resetInterval = (time) => {
-      timeLeft = time * 60;
+  const toggleTimer = () => {
+    if (isPaused) {
+      timer = setInterval(() => {
+        timeLeft--;
+        updateTimer();
+        if (timeLeft === 0) {
+          clearInterval(timer);
+          alert("¡Tiempo terminado!");
+        }
+      }, 1000);
+      isPaused = false;
+      $("#start").text("Pause");
+    } else {
       clearInterval(timer);
       isPaused = true;
       $("#start").text("Start");
-      updateTimer();
-    };
+    }
+  }
+
+  $("#short-break-btn").click(() => {
+    resetInterval(5);
   });
-})();
+
+  $("#long-break-btn").click(() => {
+    resetInterval(15);
+  });
+
+  $("#pomodoro-btn").click(() => {
+    resetInterval(25);
+  });
+
+  $("#start").click(() => toggleTimer());
+
+  $("#reset").click(() => {
+    resetInterval(25);
+  });
+
+  updateTimer();
+
+  const resetInterval = (time) => {
+    timeLeft = time * 60;
+    clearInterval(timer);
+    isPaused = true;
+    $("#start").text("Start");
+    updateTimer();
+  };
+});
