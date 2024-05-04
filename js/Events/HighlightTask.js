06cfa79d25c7caf705f91963fde7ca990d1db7f8
alfredo <alfaradayepez@gmail.com>
2024-05-04 18:28:06 -0500

    refactoring: "click" event
    Now we will only use a single click event to achieve the same objective.

--- js/Events/HighlightTask.js
+++ js/Events/HighlightTask.js
@@ -3,23 +3,19 @@
 $(function () {
 
   // Events needed to change the styles of each list item when clicked
-  $("#task-list").on("click", "li", function (e) {
+  $("#task-list").on("click", "li, li span, li p", function (e) {
     // to not highlight a task when we press finish/remove button
     if ($(e.target).is("button")) {
       return ;
     }
-
-    $("#pending-list li").removeClass("highlighted");
-    $(this).addClass("highlighted");
-  });
-
-  // This event is also necessary because it allows us to apply styles to
-  // elements that are dynamically introduced into the DOM.
-  $("#task-list").on("click", "li span, li p", function (e) {
-    e.stopPropagation();
-    if (!$(e.target).is("button")) {
-      $("#pending-list li").removeClass("highlighted");
-      $(this).closest("li").addClass("highlighted");
+    
+    // highlighted only once
+    if ($(e.target).hasClass("highlighted")) {
+      console.log("ya esta resaltado");
+      return ;
     }
+    
+    $("#pending-list li").removeClass("highlighted");
+    $(this).closest("li").addClass("highlighted");
   });
 });
