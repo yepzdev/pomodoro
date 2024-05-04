f07f398283df02964b405b823963d007031152b9
alfredo <alfaradayepez@gmail.com>
2024-05-04 16:49:53 -0500

    fix: more readable validation comment

--- js/Events/HighlightTask.js
+++ js/Events/HighlightTask.js
@@ -4,10 +4,13 @@ $(function () {
 
   // Events needed to change the styles of each list item when clicked
   $("#task-list").on("click", "li", function (e) {
-    if (!$(e.target).is("button")) {
-      $("#pending-list li").removeClass("highlighted");
-      $(this).addClass("highlighted");
+    // to not highlight a task when we press finish/remove button
+    if ($(e.target).is("button")) {
+      return ;
     }
+
+    $("#pending-list li").removeClass("highlighted");
+    $(this).addClass("highlighted");
   });
 
   // This event is also necessary because it allows us to apply styles to
