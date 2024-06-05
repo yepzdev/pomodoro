20b9634051a6efe730c91245bb4e113cf52761c9
alfredo <alfaradayepez@gmail.com>
2024-06-05 17:10:01 -0500

    prevents non-numeric values ​​in the input type field

--- js/index.js
+++ js/index.js
@@ -38,14 +38,22 @@ $(function () {
     $(".add-task-container").empty().append(addTaskButton);
   });
 
-   // Validate input to accept only numbers from 1 to 20
-   $(document).on('input', '#add-task-input', function () {
+  // Validate input to accept only numbers from 1 to 20
+  $(document).on("input", "#add-task-input", function () {
     var value = parseInt($(this).val(), 10);
     if (value < 1 || value > 20) {
-        $(this).val('');
-        alert('Please enter a number between 1 and 20');
+      $(this).val("");
+      alert("Please enter a number between 1 and 20");
     }
-});
+  });
+
+  // Prevent non-numeric input
+  $(document).on("keypress", "#add-task-input", function (e) {
+    var charCode = e.which ? e.which : e.keyCode;
+    if (charCode < 48 || charCode > 57) {
+      e.preventDefault();
+    }
+  });
 
   // finish cancel event
 
