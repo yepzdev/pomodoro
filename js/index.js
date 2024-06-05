6e9e70f16b4a1b21b7ba890bc3149aa6f13e63f8
alfredo <alfaradayepez@gmail.com>
2024-06-04 23:23:29 -0500

    Additional validation to check that the values ​​are not out of ranges with jquery, an alert is displayed if the limit is exceeded.

--- js/index.js
+++ js/index.js
@@ -38,6 +38,15 @@ $(function () {
     $(".add-task-container").empty().append(addTaskButton);
   });
 
+   // Validate input to accept only numbers from 1 to 20
+   $(document).on('input', '#add-task-input', function () {
+    var value = parseInt($(this).val(), 10);
+    if (value < 1 || value > 20) {
+        $(this).val('');
+        alert('Please enter a number between 1 and 20');
+    }
+});
+
   // finish cancel event
 
   // finish - add task compoment element
